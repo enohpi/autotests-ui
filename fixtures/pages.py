5f7ca8553c454d98dc1c6b1212eef63fd29f5646
Василиненко Вадим Сергеевич <vadim.vasilinenko@eltex.loc>
2025-09-19 17:06:53 +0700

    7.2 Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -3,6 +3,8 @@ from playwright.sync_api import Page
 
 from pages.dashboard_page import DashboardPage
 from pages.registration_page import RegistrationPage
+from pages.course_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 
 
 @pytest.fixture
@@ -13,3 +15,13 @@ def dashboard_page(chromium_page: Page) -> DashboardPage:
 @pytest.fixture
 def registration_page(chromium_page: Page) -> RegistrationPage:
     return RegistrationPage(page=chromium_page)
+
+
+@pytest.fixture
+def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
+    return CoursesListPage(page=chromium_page_with_state)
+
+
+@pytest.fixture
+def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
+    return CreateCoursePage(page=chromium_page_with_state)
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,6 +1,9 @@
 import pytest
 from playwright.sync_api import Page, expect
 
+from pages.course_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
+
 
 @pytest.mark.courses
 @pytest.mark.regression
@@ -26,3 +29,27 @@ def test_empty_courses_list(chromium_page_with_state: Page):
     expect(courses_empty_description_text).to_be_visible()
     expect(courses_empty_description_text).to_have_text(
         "Results from the load test pipeline will be displayed here")
+
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_create_course(chromium_page_with_state: Page, create_course_page: CreateCoursePage,
+                       courses_list_page: CoursesListPage):
+    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=False)
+    create_course_page.check_visible_create_course_form(title="", description="", estimated_time="", max_score="0",
+                                                        min_score="0")
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+
+    create_course_page.upload_preview_image("testdata/files/image.png")
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.fill_create_course_form(title="Playwright", description="Playwright", estimated_time="2 weeks",
+                                               max_score="100", min_score="10")
+    create_course_page.click_create_course_button()
