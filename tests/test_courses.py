df557577937d3cd9c2d1d17ef4a703bb3d4c3c55
Василиненко Вадим Сергеевич <vadim.vasilinenko@eltex.loc>
2025-09-19 17:16:15 +0700

    7.2 Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -42,7 +42,7 @@ def test_create_course(chromium_page_with_state: Page, create_course_page: Creat
     create_course_page.check_visible_image_preview_empty_view()
     create_course_page.check_disabled_create_course_button()
     create_course_page.check_visible_image_upload_view(is_image_uploaded=False)
-    create_course_page.check_visible_create_course_form(title="", description="", estimated_time="", max_score="0",
+    create_course_page.check_visible_create_course_form(title="", estimated_time="", description="", max_score="0",
                                                         min_score="0")
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
@@ -50,6 +50,12 @@ def test_create_course(chromium_page_with_state: Page, create_course_page: Creat
 
     create_course_page.upload_preview_image("testdata/files/image.png")
     create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
-    create_course_page.fill_create_course_form(title="Playwright", description="Playwright", estimated_time="2 weeks",
+    create_course_page.fill_create_course_form(title="Playwright", estimated_time="2 weeks", description="Playwright",
                                                max_score="100", min_score="10")
     create_course_page.click_create_course_button()
+
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(
+        index=0, title="Playwright", estimated_time="2 weeks", max_score="100", min_score="10"
+    )
