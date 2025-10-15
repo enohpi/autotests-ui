8ca84914bdc65b280c871e10b0948dd9e0d6cdea
Василиненко Вадим Сергеевич <vadim.vasilinenko@eltex.loc>
2025-10-15 09:41:14 +0700

    9.5 Логирование взаимодействий со страницей. Практика работы с логированием

--- elements/button.py
+++ elements/button.py
@@ -1,17 +1,27 @@
 import allure
 
-from playwright.sync_api import Page, Locator, expect
+from playwright.sync_api import expect
 
 from elements.base_element import BaseElement
+
+from tools.logger import get_logger
+
+logger = get_logger("BUTTON")
 
 
 class Button(BaseElement):
     def check_enabled(self, nth: int = 0, **kwargs):
-        with allure.step(f'Checking that {self.type_of} "{self.name}" is enabled'):
+        step = f'Checking that {self.type_of} "{self.name}" is enabled'
+
+        with allure.step(step):
+            logger.info(step)
             locator = self.get_locator(nth, **kwargs)
             expect(locator).to_be_enabled()
 
     def check_disabled(self, nth: int = 0, **kwargs):
-        with allure.step(f'Checking that {self.type_of} "{self.name}" is disabled'):
+        step = f'Checking that {self.type_of} "{self.name}" is disabled'
+
+        with allure.step(step):
+            logger.info(step)
             locator = self.get_locator(nth, **kwargs)
             expect(locator).to_be_disabled()
